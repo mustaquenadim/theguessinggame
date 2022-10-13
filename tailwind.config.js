23cbe1f6855b8e7b53742ea8712a3b20558cab2c
mustaquenadim <mustaquenadim2000@gmail.com>
2022-10-13 08:22:59 +0600

    [INIT] The Guessing Game

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif'],
+      'mulish': ['Mulish', 'sans-serif'],
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
