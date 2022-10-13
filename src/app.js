4450323f499a42dcda555567686ba8c8a504e064
mustaquenadim <mustaquenadim2000@gmail.com>
2022-10-13 11:06:29 +0600

    [Building] Logic

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const inputElem = document.getElementById("input-number");
+const guessBtn = document.getElementById("btn-guess");
+
+function myFunc(value) {
+  if (value) {
+    console.log("first", value)
+  }
+}
+
+function myBtn() {
+  myFunc();
+}
