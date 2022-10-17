ddba93a317b48ab9d4f7f647885388f8145191d4
mustaquenadim <mustaquenadim2000@gmail.com>
2022-10-18 00:01:51 +0600

    updated app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ function guessNumber(number) {
     } else {
       // console.log("Your guess is right!");
       createGuesses(numberOfGuesses, number, 'Your guess is right!');
-      success();
+      success("lose");
     }
     numberInput.value = '';
     // guessBtn.disabled = true;
@@ -33,6 +33,7 @@ function guessNumber(number) {
     remainingGuesses--;
     guessRemaining.innerText = remainingGuesses;
     console.log("Sorry! You've no guess remaining!");
+    success("lose");
   }
 }
 
@@ -78,17 +79,17 @@ function generateRandomNumber() {
 
 // Grabs all the Elements by their IDs which we had given them
 const modal = document.getElementById('modal');
-const btn = document.getElementById('open-btn');
-const playAgainBtn = document.getElementById('play-again-btn');
+const playAgainBtn = document.getElementById('playAgainBtn');
 
-function success() {
+function success(result) {
   modal.style.display = 'block';
-  message();
+  message(result);
 }
 // We want the modal to close when the OK button is clicked
-// button.onclick = function () {
-//   modal.style.display = "none";
-// }
+playAgainBtn.onclick = function () {
+  modal.style.display = "none";
+  location.reload();
+}
 
 // window.onclick = function (event) {
 //   if (event.target == modal) {
@@ -96,32 +97,34 @@ function success() {
 //   }
 // }
 
-function message(result = "win") {
+function message(result) {
+  console.log("result_____", result)
   const mod = document.createElement('div');
   mod.classList.add('modal-content');
   mod.innerHTML = `
-  <div id="modal" class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-  <div class="mt-3 text-center">
-    <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-      <svg class="h-6 w-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-      </svg>
+    <div id="modal" class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+      <div class="mt-3 text-center">
+        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full ${result === 'win' && "bg-green-100"} ${result === 'lose' && "bg-red-100"}">
+          ${result === 'win' ? (
+      `<svg class="text-green-600" fill="currentColor" stroke="none" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M18.9 35.7 7.7 24.5l2.15-2.15 9.05 9.05 19.2-19.2 2.15 2.15Z" /></svg>`
+    ) : (
+      `<svg class="text-red-600" fill="currentColor" stroke="none" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m12.55 37.05-1.6-1.6L22.4 24 10.95 12.55l1.6-1.6L24 22.4l11.45-11.45 1.6 1.6L25.6 24l11.45 11.45-1.6 1.6L24 25.6Z" /></svg>`
+    )}
+        </div>
+        <h3 class="mt-3 text-lg leading-6 font-medium text-gray-900 uppercase">${result === 'win' ? "Congratulations" : "Sorry"}</h3>
+        <div class="mt-2 px-7">
+          <p class="text-sm text-gray-500">
+            ${result === 'win' ? "Your guess is correct" : "You've lost the game"}
+          </p>
+        </div>
+        <div class="items-center px-4 mt-4">
+          <button id="playAgainBtn"
+            class="px-4 py-2 text-white text-base font-medium rounded-md shadow-sm focus:outline-none focus:ring-2 ${result === 'win' ? "bg-green-500 hover:bg-green-600 focus:ring-green-300" : 'bg-red-500 hover:bg-red-600 focus:ring-red-300'}">
+            ${result === 'win' ? 'Play Again' : 'Try Again'}
+          </button>
+        </div>
+      </div>
     </div>
-    <h3 class="mt-3 text-lg leading-6 font-medium text-gray-900 uppercase">Congratulations</h3>
-    <div class="mt-2 px-7">
-      <p class="text-sm text-gray-500">
-        Your guess is correct
-      </p>
-    </div>
-    <div class="items-center px-4 mt-4">
-      <button id="play-again-btn"
-        class="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
-        ${result === "win" ? "Play Again" : "Try Again"}
-      </button>
-    </div>
-  </div>
-</div>
   `;
   modal.appendChild(mod);
 }
